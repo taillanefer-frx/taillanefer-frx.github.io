9bb5825d17672d4b8232e9924bdadbc2e73aea49
taillanefer-frx <taillanefer@gmail.com>
2026-09-24 11:12:44 -0300

    publicar correção validada em teste real

---
+++ assets/Surface-DTq-o-d7.js
@@ -0,0 +1,1 @@
+import{j as s,d as a}from"./index-C7-zENOV.js";function d({className:r,...e}){return s.jsx("div",{className:a("rounded-3xl border border-line/80 bg-surface shadow-card",r),...e})}export{d as S};
